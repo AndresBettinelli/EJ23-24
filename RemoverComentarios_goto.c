a2b1823a40b6bc9ab9a5368a58ad38bc8a10a559
beluru <belu16_pfr@hotmail.com>
2016-11-18 05:28:15 -0300

    Create RemoverComentarios_goto.c

---
+++ RemoverComentarios_goto.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+
+/*
+ TP FINAL SSL
+ Ejercicio 1-23: Remover Comentarios
+ Grupo: 4
+ 2016
+ */
+
+main(){
+	
+	int c;
+	int d; 
+
+CODIGO:	
+	c=getchar();
+	switch(c)
+	{
+		case '/':
+			c=getchar();
+			if (c=='/'){
+				goto COMENTARIO_LINEA;
+			}else if (c=='*'){
+				goto COMENTARIO_BLOQUE;
+			} break;
+        
+		case EOF:
+     goto END;			
+		
+		case '\'':
+			putchar(c);
+			goto LITERAL_CARACTER;
+			
+		case '"':
+			putchar(c);
+			goto LITERAL_CEDENA;
+	
+		default:
+            putchar(c);
+			goto CODIGO;					
+			}  
+			
+COMENTARIO_BLOQUE:
+
+	switch(getchar())
+	{
+		case '*':
+			c=getchar();
+			if (c=='/'){
+				goto CODIGO;
+				} break;
+		default:
+            goto COMENTARIO_BLOQUE;	 
+			
+	}		
+
+COMENTARIO_LINEA:
+
+	switch(getchar())
+	{
+		case '\n':
+			goto CODIGO;
+			break;
+		
+		default:
+            goto COMENTARIO_LINEA;	 
+	}
+			
+END:
+    printf("\nLa ejecución fue exitosa");
+    return 0;
+    
+ERROR:
+	printf("\nLa ejecucion NO fue exitosa");
+	return 0;
+	    
+LITERAL_CEDENA:
+//	putchar(c);
+    switch(c=getchar())
+    {
+        case '\\':
+            //Omito el caracter escapado
+            getchar();
+            goto LITERAL_CEDENA;
+            break;
+        case '"':
+            putchar(c);
+			goto CODIGO;
+            break;
+        case '\n':
+            goto ERROR;
+            break;
+ 
+        default:
+            putchar(c);
+			goto LITERAL_CEDENA;
+            break;
+    }
+LITERAL_CARACTER:
+//    putchar(c);
+	switch(c=getchar())
+    {
+        case '\\':
+            //Omito el caracter escapado
+            getchar();
+            goto LITERAL_CARACTER;
+            break;
+        case '\'':
+            putchar(c);
+			goto CODIGO;
+            break;
+        case '\n':
+            goto ERROR;
+            break;
+        default:
+            putchar(c);
+			goto LITERAL_CARACTER;
+            break;
+    }   		
+}
